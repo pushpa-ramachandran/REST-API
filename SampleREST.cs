4ddf02837144f14282ebc24dfbe6c49c77e22ffd
Pushpa latha <pushpalatha_ram@yahoo.co.in>
2020-05-07 15:18:40 -0700

    Add the REST API files

---
+++ SampleREST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace IIdPasswordService
+{
+    [ServiceContract]
+    public interface IIdPasswordService
+    {
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/passwordCreate?firstName={firstName}&lastName={lastName}&age={age}")]             // define input format
+        Results passwordCreate(string firstName, string lastName, int age);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/loginId?age={age}")]             // define input format
+        int loginId(int age);
+
+    }
+        [DataContract]
+        public class Results
+        {
+            [DataMember]
+            public string fn { get; set; }
+            [DataMember]
+            public string ln { get; set; }
+
+            [DataMember]
+            public int id { get; set; }
+        }
+    }
+
+   
+
---
+++ SampleREST.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace IIdPasswordService
+{
+    public class Service1 : IIdPasswordService
+    {
+        public Results passwordCreate( string firstName, string lastName, int age)
+        {
+            
+            Results res = new Results();
+            string fn = "", ln = "";
+            int id = 0;
+            ln = lastName.Substring(0, 2);
+            fn = firstName.Substring(firstName.Length - 2);
+            id = age % 5;
+
+ 
+            res.fn = fn;
+            res.ln = ln;
+            res.id = id;
+            return res;
+        }
+
+        public int loginId(int age)
+        {
+
+            return (age * new Random().Next(100, 200));
+        }
+
+
+    }
+}
